fb40ac8c72d86c488302281c60a2480b2b548a34
edvard01 <frejaclueless@gmail.com>
2024-02-22 14:07:26 +0100

    Added more styling, more songs in the playlist, changed some styling in the playlist component

--- src/Playlist.tsx
+++ src/Playlist.tsx
@@ -6,12 +6,114 @@ export function Playlist(): JSX.Element {
       <div className="playlist">
         <ul>
           <li>
-            <img src="src\assets\WageWarManic.jpg" alt="IMG" />
-            <span className="song-text">
-              <h4>Song Title</h4>
-              <p>artist</p>
-            </span>
-            <span className="material-symbols-outlined"> play_circle </span>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Relapse</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Teeth</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Manic</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>High Horse</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Circle The Drain</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Godspeed</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Death Roll</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Slow Burn</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>Never Said Goodbye</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>True Colors</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
+          </li>
+          <li>
+            <div className="li-content">
+              <img src="src\assets\WageWarManic.jpg" alt="IMG" />
+              <span className="song-text">
+                <h4>If Tomorrow Never Comes</h4>
+                <p>Wage War</p>
+              </span>
+              <span className="material-symbols-outlined"> play_circle </span>
+            </div>
           </li>
         </ul>
       </div>
